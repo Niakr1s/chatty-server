f16d040e7ec8d47fc35a01f4dac029ef160498b1
Niakr1s <pavel2188@gmail.com>
2020-05-23 01:01:49 +0300

    moved Chat errors into chat

--- app/db/logged/errors.go
+++ app/db/logged/errors.go
@@ -7,9 +7,3 @@ var (
 	ErrAlreadyLogged = errors.New("user is already logged in")
 	ErrNotLogged     = errors.New("user not logged in")
 )
-
-// User errors
-var (
-	ErrAlreadyInChat = errors.New("user is already logged in chat")
-	ErrNotInChat     = errors.New("user not logged in such chat")
-)
