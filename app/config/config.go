18d73d536ba4591146ead6e3ca0375270d670129
Niakr1s <pavel2188@gmail.com>
2020-05-22 21:25:43 +0300

    added config

---
+++ app/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+// Config ...
+type Config struct {
+	ServerListenAddress string
+}
+
+// C contains configuration for app
+// Config filename : "config.toml"
+var C Config
+
+const configFilepath = "config.toml"
+
+func init() {
+	if _, err := toml.DecodeFile(configFilepath, &C); err != nil {
+		panic(err)
+	}
+	log.Printf("config file succesfully loaded from %s", configFilepath)
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module server2
 go 1.14
 
 require (
+	github.com/BurntSushi/toml v0.3.1
 	github.com/go-playground/validator/v10 v10.3.0
-	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/gorilla/mux v1.7.4
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 )
