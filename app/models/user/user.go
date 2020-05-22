cedf4593afc1fd32ca899bb1b5d65dd4ca1f6970
Niakr1s <pavel2188@gmail.com>
2020-05-22 20:19:55 +0300

    added User

---
+++ app/models/user/user.go
@@ -0,0 +1,69 @@
+package user
+
+import (
+	"server2/app/validator"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User ...
+type User struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name" validate:"required"`
+
+	PasswordHash string `json:"=" validate:"required"`
+	Password     string `json:"password,omitempty" validate:"gt=5,lt=15"`
+}
+
+// ValidateBeforeStoring used before storing in database
+func (u *User) ValidateBeforeStoring() error {
+	return validator.Validate.Struct(*u)
+}
+
+// GeneratePasswordHash ...
+func (u *User) GeneratePasswordHash() error {
+	if u.Password == "" {
+		return ErrPasswordIsEmpty
+	}
+
+	hash, err := generatePasswordHash(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	u.PasswordHash = hash
+	return nil
+}
+
+// CheckPassword ...
+func (u *User) CheckPassword(password string) error {
+	if u.PasswordHash == "" {
+		err := u.GeneratePasswordHash()
+		if err != nil {
+			return err
+		}
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generatePasswordHash(pass string) (string, error) {
+	if pass == "" {
+		return "", ErrPasswordIsEmpty
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.MinCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
---
+++ app/models/user/user_test.go
@@ -0,0 +1,159 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUser_GeneratePasswordHash(t *testing.T) {
+	tests := []struct {
+		name    string
+		u       *User
+		wantErr bool
+	}{
+		{
+			"valid password",
+			&User{Password: "validpassword"},
+			false,
+		},
+		{
+			"empty password",
+			&User{Password: ""},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.u.GeneratePasswordHash()
+
+			assert.Equal(t, err != nil, tt.wantErr)
+		})
+	}
+}
+
+func Test_generatePasswordHash(t *testing.T) {
+	type args struct {
+		pass string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"valid password",
+			args{"validpassword"},
+			false,
+		},
+		{
+			"empty password",
+			args{""},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := generatePasswordHash(tt.args.pass)
+			assert.Equal(t, err != nil, tt.wantErr)
+		})
+	}
+}
+
+func TestUser_CheckPassword(t *testing.T) {
+	type args struct {
+		password string
+	}
+	tests := []struct {
+		name    string
+		u       *User
+		args    args
+		wantErr bool
+	}{
+		{
+			"same password",
+			GenerateTestUser(t),
+			args{"password"},
+			false,
+		},
+		{
+			"other password",
+			GenerateTestUser(t),
+			args{"otherpassword"},
+			true,
+		},
+		{
+			"empty password",
+			GenerateTestUser(t),
+			args{""},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.u.GeneratePasswordHash()
+			err := tt.u.CheckPassword(tt.args.password)
+			assert.Equal(t, err != nil, tt.wantErr, err)
+		})
+	}
+}
+
+func GenerateTestUser(t *testing.T) *User {
+	t.Helper()
+	return &User{Name: "user", Password: "password"}
+}
+
+func TestUser_ValidateBeforeStoring(t *testing.T) {
+	type fields struct {
+		Name         string
+		PasswordHash string
+		Password     string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			"valid",
+			fields{"user", "hash", "password"},
+			false,
+		},
+		{
+			"short pass",
+			fields{"user", "hash", "sh"},
+			true,
+		},
+		{
+			"large pass",
+			fields{"user", "hash", "superlargepasswordlol"},
+			true,
+		},
+		{
+			"no user",
+			fields{"", "hash", "password"},
+			true,
+		},
+		{
+			"no hash",
+			fields{"user", "", "password"},
+			true,
+		},
+		{
+			"no pass",
+			fields{"user", "hash", ""},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &User{
+				Name:         tt.fields.Name,
+				PasswordHash: tt.fields.PasswordHash,
+				Password:     tt.fields.Password,
+			}
+			err := u.ValidateBeforeStoring()
+			assert.Equal(t, (err != nil), tt.wantErr)
+		})
+	}
+}
