ef0ebebec9b3e6179e1a3eed8aff92a27a37bddf
Niakr1s <pavel2188@gmail.com>
2020-06-01 22:50:15 +0300

    fixed poll to wait fixed time

--- app/server/poll.go
+++ app/server/poll.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/niakr1s/chatty-server/app/events"
 	"github.com/niakr1s/chatty-server/app/internal/httputil"
@@ -13,15 +14,14 @@ import (
 func (s *Server) Poll(w http.ResponseWriter, r *http.Request) {
 	username := sess.GetUserNameFromCtx(r.Context())
 
-	event, ok := <-s.pool.GetUserChan(username)
-	if !ok {
-		return
-	}
+	select {
+	case event := <-s.pool.GetUserChan(username):
+		ewt := events.NewEventWithType(event)
 
-	ewt := events.NewEventWithType(event)
-
-	if err := json.NewEncoder(w).Encode(ewt); err != nil {
-		httputil.WriteError(w, err, http.StatusInternalServerError)
-		return
+		if err := json.NewEncoder(w).Encode(ewt); err != nil {
+			httputil.WriteError(w, err, http.StatusInternalServerError)
+			return
+		}
+	case <-time.After(time.Second * 10):
 	}
 }
