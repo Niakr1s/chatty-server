c682cb62e97912a4a0e673fab816eadbc421b659
Niakr1s <pavel2188@gmail.com>
2020-06-06 10:52:49 +0300

    added test for JoinChatEvent UserStatus

--- app/db/notify/chat_test.go
+++ app/db/notify/chat_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/niakr1s/chatty-server/app/db/memory"
 	"github.com/niakr1s/chatty-server/app/events"
+	"github.com/niakr1s/chatty-server/app/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,4 +31,20 @@ func TestChat_notify(t *testing.T) {
 		assert.Fail(t, "channel should be empty")
 	default:
 	}
+}
+
+func TestChat_NotifyJoinChatWithUserStatus(t *testing.T) {
+	logged := memory.NewLoggedDB()
+	lu, _ := logged.Login(username)
+	lu.UserStatus = models.UserStatus{Admin: true, Verified: true}
+	logged.Update(lu)
+
+	ch := make(chan events.Event)
+	chat := NewChat(memory.NewChat(chatname), logged, ch)
+	chat.AddUser(username)
+
+	joinE := (<-ch).(*events.ChatJoinEvent)
+	assert.Equal(t, joinE.UserName, username)
+	assert.Equal(t, true, joinE.Verified)
+	assert.Equal(t, true, joinE.Admin)
 }
