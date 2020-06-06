da434f9c9f6e3f0e554e3f6d5d080d88331ee2da
Niakr1s <pavel2188@gmail.com>
2020-06-06 13:42:46 +0300

    ChatEvent is global

--- app/events/chatEvents.go
+++ app/events/chatEvents.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/niakr1s/chatty-server/app/er"
 	"github.com/niakr1s/chatty-server/app/models"
 )
 
-// ChatEvent ...
+// ChatEvent is always global event.
 type ChatEvent struct {
 	models.Chat
 	Time models.UnixTime
@@ -20,6 +21,11 @@ func NewChatEvent(chatname string, t time.Time) *ChatEvent {
 
 func (ce *ChatEvent) String() string {
 	return fmt.Sprintf("chat: %s, time: %v", ce.ChatName, ce.Time)
+}
+
+// InChat global event.
+func (ce *ChatEvent) InChat() (string, error) {
+	return "", er.ErrGlobalEvent
 }
 
 // ChatCreatedEvent ...
