f7c7760789bb73ee72714ff843097146b726cfc7
Niakr1s <pavel2188@gmail.com>
2020-06-10 10:24:04 +0300

    changed HelpCommand

--- app/bot/internal/command/helpCommand.go
+++ app/bot/internal/command/helpCommand.go
@@ -3,6 +3,6 @@ package command
 // HelpCommand for string "/help"
 func HelpCommand() CommandFunc {
 	return func() (string, error) {
-		return `Available commands: /help, /anecdot, /anekdot`, nil
+		return `Available commands: /help, /anecdot (/anekdot)`, nil
 	}
 }
